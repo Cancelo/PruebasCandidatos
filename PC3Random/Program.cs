6621f8650b79357c78af2b4941cea563d1ce97b4
Cancelo <kancelo@gmail.com>
2023-07-22 10:48:01 +0200

    Add project files.

---
+++ PC3Random/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PC3Random
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
