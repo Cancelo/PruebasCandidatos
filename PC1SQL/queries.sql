0c9e82b1f15746f0a3f1f04ece430383f24aa285
Cancelo <kancelo@gmail.com>
2023-07-22 11:11:49 +0200

    Estructura solución

---
+++ PC1SQL/queries.sql
@@ -0,0 +1,11 @@
+-- 1. Obtener la lista de los productos no descatalogados incluyendo el nombre de la categoría ordenado por nombre de producto.
+
+
+-- 2. Mostrar el nombre de los clientes de Nancy Davolio.
+
+
+-- 3. Mostrar el total facturado por año del empleado Steven Buchanan.
+
+
+-- 4. Mostrar el nombre de los empleados que dependan directa o indirectamente de Andrew Fuller.
+
---
+++ PC4Customers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PC4Customers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
