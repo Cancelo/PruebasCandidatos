5c31c10e20f4ec286915e617a96f8ca419cce290
Cancelo <kancelo@gmail.com>
2023-07-22 13:11:49 +0200

    Add PC3

--- PC1SQL/queries.sql
+++ PC1SQL/queries.sql
@@ -1,8 +1,17 @@
 -- 1. Obtener la lista de los productos no descatalogados incluyendo el nombre de la categoría ordenado por nombre de producto.
-
+SELECT Products.ProductName, Categories.CategoryName 
+FROM Products 
+INNER JOIN Categories on Products.CategoryID = Categories.CategoryID
+WHERE Products.Discontinued = 0
+ORDER BY Products.ProductName ASC;
 
 -- 2. Mostrar el nombre de los clientes de Nancy Davolio.
-
+SELECT DISTINCT Customers.ContactName
+FROM Employees
+INNER JOIN Orders ON Employees.EmployeeID = Orders.EmployeeID
+INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID
+WHERE Employees.FirstName = 'Nancy' AND Employees.LastName = 'Davolio'
+ORDER BY Customers.ContactName ASC;
 
 -- 3. Mostrar el total facturado por año del empleado Steven Buchanan.
 
--- PC3Random/Program.cs
+++ PC3Random/Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 
 namespace PC3Random
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static int totalThreads = 1;
+        private static int totalNumbers = 10000000 / totalThreads;
+        private static HashSet<int> numbers = new HashSet<int>(totalNumbers);
+        private static string path = "";
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var timer = new Stopwatch();
+
+            timer.Start();
+            Console.WriteLine($"[{timer.Elapsed}] Inicio");
+
+            GenerateNumbers();
+            Console.WriteLine($"[{timer.Elapsed}] Números generados: {numbers.Count} ({totalNumbers - numbers.Distinct().Count()} repetidos)");
+
+            CreateFile();
+            Console.WriteLine($"[{timer.Elapsed}] Archivo creado: {path}");
+
+            timer.Stop();
+            Console.WriteLine($"[{timer.Elapsed}] Fin");
+        }
+
+        private static void GenerateNumbers()
+        {
+            Random random = new Random();
+            int index = 0;
+            while (index < totalNumbers)
+            {
+                if (numbers.Add(random.Next(int.MaxValue))) index++;
+            }
+        }
+
+        private static void CreateFile()
+        {
+            try
+            {
+                File.WriteAllText("output.txt", String.Join(",", numbers));
+
+                if (File.Exists("output.txt"))
+                {
+                    FileInfo file = new FileInfo("output.txt");
+                    path = file.FullName;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error al crear el archivo: {ex.Message}");
+            }
         }
     }
 }
